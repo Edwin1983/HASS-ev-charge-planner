a17c3618d42c6773a23609ef12d6063ca2a9cef8
Edwin1983 <119804527+Edwin1983@users.noreply.github.com>
2026-09-20 21:01:39 +0200

    Update tests for translated sensor entity IDs

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -82,8 +82,8 @@ async def test_full_integration_setup_and_unload(hass):
     states = hass.states
     assert states.get("sensor.ev_planner_state") is not None
     assert states.get("sensor.ev_planner_data") is not None
-    assert states.get("sensor.ev_planner_gewenste_laadstroom") is not None
-    assert states.get("sensor.ev_planner_gewenste_fase") is not None
+    assert states.get("sensor.ev_planner_desired_charge_current") is not None
+    assert states.get("sensor.ev_planner_desired_phases") is not None
     assert states.get("binary_sensor.ev_planner_charging_allowed") is not None
     assert states.get("switch.ev_planner_smart_charging") is not None
     assert states.get("select.ev_charge_planner_departure_day") is not None
@@ -250,8 +250,8 @@ async def test_full_planning_chain(hass):
     assert data_state.attributes["decisions"]
     assert data_state.attributes["energy_planned_kwh"] > 0
 
-    charge_current = hass.states.get("sensor.ev_planner_gewenste_laadstroom")
-    phases = hass.states.get("sensor.ev_planner_gewenste_fase")
+    charge_current = hass.states.get("sensor.ev_planner_desired_charge_current")
+    phases = hass.states.get("sensor.ev_planner_desired_phases")
     assert charge_current is not None
     assert phases is not None
     assert 0 <= int(charge_current.state) <= 16
